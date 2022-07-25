573eaa71dc5a22a424db289ead0641ecb47703f4
William A <wsashbee@gmail.com>
2022-07-25 13:58:23 -0400

    cleanup

--- docker/pytorch3d/pytorch3d/common/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .types import Device, make_device, get_device
-
-__all__ = [k for k in globals().keys() if not k.startswith("_")]

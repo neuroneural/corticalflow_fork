1eb8b276ff76533db5961d5f7d694f3f76d95c0e
William Ashbee <wsashbee@gmail.com>
2022-11-12 13:43:00 -0500

    training for arctic

--- singularity/submittrainlhwhite.sh
+++ singularity/submittrainlhwhite.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -c 5
+#SBATCH -c 4
 #SBATCH --mem=30g
 #SBATCH -p qTRDGPUH
-#SBATCH --gres=gpu:v100:1
-#SBATCH --nodelist=trendsdgx003.rs.gsu.edu 
+#SBATCH --gres=gpu:V100:1
 #SBATCH -t 3-00:00
-#SBATCH -J corticalfl
-#SBATCH -e /data/users2/washbee/corticalflow/jobs/error%A.err
-#SBATCH -o /data/users2/washbee/corticalflow/jobs/out%A.out
-#SBATCH -A PSYC0002
+#SBATCH -A psy53c17
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=washbee1@student.gsu.edu
 #SBATCH --oversubscribe
+#SBATCH --exclude=arctrdgn002
+#SBATCH -J corticalfl
+#SBATCH -e /data/users2/washbee/corticalflow/jobs/error%A.err
+#SBATCH -o /data/users2/washbee/corticalflow/jobs/out%A.out
 
 sleep 5s
 
-singularity exec --nv --bind $HOME/projects/corticalflow:/corticalflow,/data:/data,/data/users2/washbee/outdir:/subj /data/users2/washbee/containers/corticalflow3.sif /corticalflow/singularity/trainlhwhite.sh &
+module load singularity/3.10.2
+singularity exec --nv --bind /data/users2/washbee/speedrun/corticalflow_fork:/corticalflow,/data:/data,/data/users2/washbee/outdir:/subj /data/users2/washbee/containers/speedrun/cfpp_sr.sif /corticalflow/singularity/trainlhwhite.sh &
 
 wait
 

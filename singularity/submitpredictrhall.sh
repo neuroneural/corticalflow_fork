717cba5ff144583771015133452b713d41dc3b69
William Ashbee <wsashbee@gmail.com>
2022-11-29 20:16:32 +0000

    test split update, and submit predict singularity script updates

--- singularity/submitpredictrhall.sh
+++ singularity/submitpredictrhall.sh
@@ -3,20 +3,23 @@
 #SBATCH -c 5
 #SBATCH --mem=30g
 #SBATCH -p qTRDGPUH
-#SBATCH --gres=gpu:v100:1
-#SBATCH --nodelist=trendsdgx004.rs.gsu.edu 
+#SBATCH --gres=gpu:V100:1
 #SBATCH -t 1-00:00
-#SBATCH -J corticalfpredl
+#SBATCH -J cfpredr
 #SBATCH -e /data/users2/washbee/corticalflow/jobs/error%A.err
 #SBATCH -o /data/users2/washbee/corticalflow/jobs/out%A.out
-#SBATCH -A PSYC0002
+#SBATCH -A psy53c17
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=washbee1@student.gsu.edu
 #SBATCH --oversubscribe
+#SBATCH --exclude=arctrdgn002,arctrddgx001
 
 sleep 5s
 
-singularity exec --nv --bind $HOME/projects/corticalflow:/corticalflow,/data:/data,/data/users2/washbee/outdir:/subj /data/users2/washbee/containers/corticalflow3.sif /corticalflow/singularity/predictrhall.sh &
+source /usr/share/lmod/lmod/init/bash
+module use /application/ubuntumodules/localmodules
+module load singularity/3.10.2
+singularity exec --nv --bind /data/users2/washbee/speedrun/corticalflow_fork:/corticalflow,/data,/data/users2/washbee/outdir:/subj /data/users2/washbee/containers/speedrun/cfpp_sr.sif /corticalflow/singularity/predictrhall.sh &
 
 wait
 
